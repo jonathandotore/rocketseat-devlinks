a819523c65e6aa05d67bbe10d4ff11042e9f7a4f
Jonathan Dotore <61588081+jonathandotore@users.noreply.github.com>
2023-01-23 22:04:41 -0300

    Adicionando arquivos do projeto.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // if(html.classList.contains('light')) {
+    //     html.classList.remove('light')
+    // }
+    // else {
+    //     html.classList.add('light')
+    // }
+
+
+    /* Substituindo imagem */
+
+    /* Se tema = light mode, adicionar imagem ligh */
+}
+
